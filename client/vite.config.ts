7875a600931efb0aa2d9d3d0af3333b3c3b0d17d
FranciscoPoli <124818536+FranciscoPoli@users.noreply.github.com>
2025-05-18 15:21:09 +0000

    build changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,14 +4,21 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: "./",
   build: {
-    outDir: "dist",
+    outDir: "../dist",
     emptyOutDir: true
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
     }
   }
 });
